a22ebde8378c5058d141a7e1b6d9b29b3b5dd4b1
Gabriel Rigonato <grigonato@icloud.com>
2023-04-18 21:33:38 -0300

    First Function

--- scripts/main.c
+++ scripts/main.c
@@ -1,8 +1,20 @@
-#include <iostream>
+#include <stdio.h>
 
-int main() {
+int main()
+{
+  int x;
 
-  std::cout << "Hello world";
+  printf("Escolha as opcoes 1 ou 2: \n");
+  scanf("%d", &x);
 
+  if(x == 1) {
+    printf("Jogar\n");
+  } else
+  if (x == 2)
+  {
+     printf("Sair\n");
+  }
+  
+  system("pause");
   return 0;
 }

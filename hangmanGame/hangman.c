b209afa860740b78d894e38a8e9dd6cf847553af
WashingtonBDS <washington.b400@gmail.com>
2023-04-19 09:23:59 -0300

    menu

---
+++ hangmanGame/hangman.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <time.h>
+#include <locale.h>
+
+int main (void) {
+
+    int menu_opcao
+
+     printf("\n\n\n\t\t************************************\n");
+        printf("\t\t******** JOGO DA FORCA EM C ********\n");
+        printf("\t\t************************************\n\n");
+
+        printf("\t     +---------------------------------------+\n");
+        printf("\t     |   [1] - JOGAR                         |\n");
+        printf("\t     |   [2] - SAIR                          |\n");
+        printf("\t     +---------------------------------------+\n\n\n");
+        printf("\t\t\t\t\t\tV1.0.0.5\n");
+        printf("\t\t  ");
+
+        scanf("%d", &menu_opcao);
+}
--- scripts/main.c
+++ scripts/main.c
@@ -1,8 +1,16 @@
 #include <iostream>
 
+
 int main() {
 
   std::cout << "Hello world";
 
   return 0;
 }
+
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// int main() {
+//     printf("Hello, world.\n");
+// }
